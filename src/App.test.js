3bcc52f480a7a7f5f72ec031ab69dc60d70ef7be
pookieboot <oulaalte@gmail.com>
2025-01-27 09:06:58 +0000

    updated the test file

--- src/App.test.js
+++ src/App.test.js
@@ -9,13 +9,15 @@ import { App } from './App';
  */
 test('App should render', () => {
   render(<App />);
-
   expect(screen.getByText('Welcome, party people!')).toBeInTheDocument();
 });
 
+/**
+ * Verify button should render
+ */
 test('Button should render', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  expect(screen.getByText('Current theme: light')).toBeInTheDocument();
 });
 
 /**
@@ -23,16 +25,24 @@ test('Button should render', () => {
  * hint: use fireEvent.click(element) to trigger a click event on an element
  */
 test('theme button should update button text', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  const button = screen.getByText('Current theme: light');
+  fireEvent.click(button);
+  expect(button).toHaveTextContent('Current theme: dark');
 });
 
-// BONUS
-// hint: there is a `.toHaveStyle` method.
-// e.g.: expect(element).toHaveStyle('color: #FFF');
+/**
+ * Verify clicking button should toggle styles
+ * hint: there is a .toHaveStyle method.
+ * e.g.: expect(element).toHaveStyle('color: #FFF');
+ */
 test('theme button should toggle styles', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  const button = screen.getByText('Current theme: light');
+  fireEvent.click(button);
+  expect(document.body).toHaveStyle('background-color: #333');
+  fireEvent.click(button);
+  expect(document.body).toHaveStyle('background-color: #FFF');
 });
 
 /**
@@ -41,18 +51,22 @@ test('theme button should toggle styles', () => {
  * hint: you can check if something does not exist by using .not
  * e.g. expect(element).not.toBeInTheDocument()
  *
- * hint: use `queryByText` instead of `getByText` to check if something is _not_ rendered
+ * hint: use queryByText instead of getByText to check if something is _not_ rendered
  * (getByText will throw an error if it is not rendered)
  */
 test('hidden button should toggle hidden content', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  const button = screen.getByText('Show hidden content');
+  fireEvent.click(button);
+  expect(screen.getByText('this content is hidden by default')).toBeInTheDocument();
+  fireEvent.click(button);
+  expect(screen.queryByText('this content is hidden by default')).not.toBeInTheDocument();
 });
-
 
 /**
  * Want more? Try these:
  *   - check for the presence of a specific element, like the paragraph containing the text "Click the button to toggle the theme"
- *   - check the for the class name .container on the surrounding div
+ *   - check the class name .container on the surrounding div
  *   - after clicking the toggle hidden content button, check for the button text to update to "hide" instead of "show"
  */
+
